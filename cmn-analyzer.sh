69605494709918b8c37fb3e1a3540f8e01bb7817
Yibo Cai <yibo.cai@arm.com>
2024-11-13 09:20:58 +0800

    reload cmn driver after done

--- cmn-analyzer.sh
+++ cmn-analyzer.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
-exec python3 "$(dirname "$0")/cmn-analyzer" "$@"
+: ${RELOAD_CMN:=no}
+
+reload_cmn_driver_on_exit() {
+    if [ ${RELOAD_CMN} = no ]; then
+        echo "NOTE: this program may break cmn driver, reload arm_cmn driver" \
+             "if it behaves abnormal, e.g., outputs all zero"
+    else
+        if lsmod | grep arm_cmn &> /dev/null; then
+            echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+            echo "reload cmn driver on exit"
+            if ! sudo rmmod arm_cmn; then
+                echo "failed to unload cmn driver!"
+            elif ! sudo modprobe arm_cmn; then
+                echo "failed to load cmn driver!"
+            else
+                echo "done"
+            fi
+            echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+        fi
+    fi
+}
+
+trap "reload_cmn_driver_on_exit" EXIT
+python3 "$(dirname "$0")/cmn-analyzer" "$@"
