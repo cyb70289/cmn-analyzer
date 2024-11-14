25b3f5f6af6566c5510b92a745e0c1bd3e165982
Yibo Cai <yibo.cai@arm.com>
2024-11-14 09:21:54 +0000

    refine cmn driver warning message

--- cmn-analyzer.sh
+++ cmn-analyzer.sh
@@ -3,12 +3,12 @@
 : ${RELOAD_CMN:=no}
 
 reload_cmn_driver_on_exit() {
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
     if [ ${RELOAD_CMN} = no ]; then
-        echo "NOTE: this program may break cmn driver, reload arm_cmn driver" \
-             "if it behaves abnormal, e.g., outputs all zero"
+        echo "this program may break cmn driver, unload and reload arm_cmn"
+        echo "kernel module if it behaves abnormal, e.g., outputs all zero"
     else
         if lsmod | grep arm_cmn &> /dev/null; then
-            echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
             echo "reload cmn driver on exit"
             if ! sudo rmmod arm_cmn; then
                 echo "failed to unload cmn driver!"
@@ -17,10 +17,25 @@ reload_cmn_driver_on_exit() {
             else
                 echo "done"
             fi
-            echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
         fi
     fi
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 }
 
-trap "reload_cmn_driver_on_exit" EXIT
+write_cmn=0
+show_help=0
+for arg in "$@"; do
+    case ${arg} in
+        info|stat|trace):
+            write_cmn=1
+            ;;
+        -h|--help):
+            show_help=1
+            ;;
+    esac
+done
+if [ ${write_cmn} = 1 ] && [ ${show_help} = 0 ]; then
+    trap "reload_cmn_driver_on_exit" EXIT
+fi
+
 python3 "$(dirname "$0")/cmn-analyzer" "$@"
